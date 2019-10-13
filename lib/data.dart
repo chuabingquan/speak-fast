ed5e98b18338622a63eddca944a9b19c73f777b6
Chua Bing Quan <chipneverdies@gmail.com>
2019-10-13 11:43:07 +0800

    Update data

--- lib/data.dart
+++ lib/data.dart
@@ -1,7 +1,8 @@
 import './models/reading.dart';
 
 final List<Reading> READINGS = const [
-  Reading("The real problem is that programmers have spent far too much time worrying about efficiency in the wrong places and at the wrong times, premature optimization is the root of all evil in programming", 5),
+  Reading("Nobody exists on purpose, nobody belongs anywhere, everybody’s gonna die. Come watch TV", 2),
   Reading("Gucci gang, Gucci gang, Gucci gang, Gucci gang Gucci gang, Gucci gang, Gucci gang", 3),
   Reading("How much wood would a woodchuck chuck if a woodchuck could chuck wood", 5),
+  Reading("SECURE THE BORDER! BUILD A WALL!", 2),
 ];
